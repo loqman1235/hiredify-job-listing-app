0948f0c84376b1d9803099c20b72f24678fc57f5
loqman1235 <tonyvito12@gmail.com>
2024-09-17 12:26:13 +0100

    commit

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="grid min-h-screen w-full place-content-center bg-gradient-to-br from-[var(--gradient-blue-1)] from-0% via-[var(--gradient-blue-2)] via-[24%] to-[var(--gradient-blue-1)] to-100% p-5">
       <div className="mb-5 text-center">
-        <Brand />
+        <Brand className="text-xl" />
       </div>
       <div className="w-[480px] max-w-[90vw] rounded-xl bg-foreground p-10 shadow-sm">
         {children}
