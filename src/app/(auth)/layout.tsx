fce2b0c5ec561eb1e6f3b30af001b399124c5e5d
loqman1235 <tonyvito12@gmail.com>
2024-09-17 11:03:01 +0100

    commit

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="grid h-screen w-full place-content-center bg-gradient-to-br from-[var(--gradient-blue-1)] from-0% via-[var(--gradient-blue-2)] via-[24%] to-[var(--gradient-blue-1)] to-100%">
-      <div className="min-w-[480px] rounded-xl bg-foreground p-10 shadow-sm">
+    <main className="grid min-h-screen w-full place-content-center bg-gradient-to-br from-[var(--gradient-blue-1)] from-0% via-[var(--gradient-blue-2)] via-[24%] to-[var(--gradient-blue-1)] to-100% p-5">
+      <div className="w-[480px] max-w-[90vw] rounded-xl bg-foreground p-10 shadow-sm">
         {children}
       </div>
     </main>
