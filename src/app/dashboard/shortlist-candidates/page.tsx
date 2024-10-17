70e60cd641f6ace677633b052ed1f32cb2d1e45f
loqman1235 <tonyvito12@gmail.com>
2024-10-17 12:33:26 +0100

    commit

---
+++ src/app/dashboard/shortlist-candidates/page.tsx
@@ -0,0 +1,11 @@
+const ShortlistCandidatesPage = () => {
+  return (
+    <div>
+      <h3 className="mb-5 text-2xl font-bold tracking-tight">
+        Candidates Shortlist
+      </h3>
+    </div>
+  );
+};
+
+export default ShortlistCandidatesPage;
